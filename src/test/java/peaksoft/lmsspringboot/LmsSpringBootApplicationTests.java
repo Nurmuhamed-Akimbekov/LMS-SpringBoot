52e4a89ff90cae3320a187139bfda009e14e3b7d
nurmuhamedakimbekov <nurmur280899@gmail.com>
2024-06-04 19:42:47 +0600

    Initial commit

---
+++ src/test/java/peaksoft/lmsspringboot/LmsSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package peaksoft.lmsspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmsSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
